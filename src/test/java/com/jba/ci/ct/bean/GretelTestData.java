a84aaae14428df9b7aa2c4ddcca0a32196f9ffff
Jude Antony <judebantony@gmail.com>
2022-02-22 12:31:43 +0530

    JUDE-80 updated teh annotation

--- src/test/java/com/jba/ci/ct/bean/GretelTestData.java
+++ src/test/java/com/jba/ci/ct/bean/GretelTestData.java
@@ -2,12 +2,14 @@ package com.jba.ci.ct.bean;
 
 import com.opencsv.bean.CsvBindByPosition;
 
+import lombok.Data;
+@Data
 public class GretelTestData {
 	@CsvBindByPosition(position = 0)
-    private String episode;
+    private Integer episode;
 
     @CsvBindByPosition(position = 1)
-    private String pitch_no;
+    private Integer pitch_no;
     
     @CsvBindByPosition(position = 2)
     private String idea;
